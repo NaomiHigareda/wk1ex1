d4b0c6aad7db187005fe32e0ca023c4b36cdfd48
NaomiHigareda <nfhigareda@amarillocollege.com>
2025-01-18 20:22:58 -0600

    Welcome Note

--- wk1ex1/Program.cs
+++ wk1ex1/Program.cs
@@ -1,2 +1,17 @@
-﻿// Welcome note
-Console.WriteLine("Welcome to ITSE1302");
+﻿
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace wk1ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to class ITSE1302");
+        }
+    }
+}
